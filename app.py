1e38359ec9102a5cda30eb9fcb7f2bbe9c2e466a
Dea Puspita Anggraeni <125977899+anggraenideaa@users.noreply.github.com>
2024-05-07 15:13:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/anggraenideaa/data-visual-2024/master/tips.csv")
 
+
 # printing the top 10 rows
-display(data.head(10))
+st.dataframe(data.head(10))
