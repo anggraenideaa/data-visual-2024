21563f0d4256ee1dd28c002ecbc55d358c42cc2e
Dea Puspita Anggraeni <125977899+anggraenideaa@users.noreply.github.com>
2024-05-07 15:22:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 # Menampilkan teks statis
 st.markdown("<h3>Visualisasi Data dengan data Tips.csv</h3>", unsafe_allow_html=True)
 
+#VISUALISASI 1
 # reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/anggraenideaa/data-visual-2024/master/tips.csv")
 
@@ -22,6 +23,30 @@ ax.set_ylabel('Tip')
 
 # Display the plot in Streamlit
 st.pyplot(fig)
+
+
+#VISUALISASI 2
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# reading the database
+data = pd.read_csv("tips.csv")
+
+# Scatter plot with day against tip
+plt.scatter(data['day'], data['tip'], c=data['size'], 
+			s=data['total_bill'])
+
+# Adding Title to the Plot
+plt.title("Scatter Plot")
+
+# Setting the X and Y labels
+plt.xlabel('Day')
+plt.ylabel('Tip')
+
+plt.colorbar()
+
+plt.show()
 
 
 # # reading the database
