8959961fffed12f3f70189d5467e877bd7b284d4
Dea Puspita Anggraeni <125977899+anggraenideaa@users.noreply.github.com>
2024-05-07 15:12:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,5 @@ import matplotlib.pyplot as plt
 # Reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/anggraenideaa/data-visual-2024/master/tips.csv")
 
-# Create a figure and axis objects
-fig, ax = plt.subplots()
-
-# Scatter plot with size against tip
-ax.scatter(data['size'], data['tip'])
-
-# Adding Title to the Plot
-ax.set_title("Scatter Plot")
-
-# Setting the X and Y labels
-ax.set_xlabel('Size')
-ax.set_ylabel('Tip')
-
-# Show the plot
-plt.show()
+# printing the top 10 rows
+display(data.head(10))
