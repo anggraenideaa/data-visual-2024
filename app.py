4b66f8c3176fc6e3cce22b29ca6162fc24cda7e9
Dea Puspita Anggraeni <125977899+anggraenideaa@users.noreply.github.com>
2024-05-07 15:14:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-
-import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Reading the database
+
+# reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/anggraenideaa/data-visual-2024/master/tips.csv")
 
+# Scatter plot with day against tip
+plt.scatter(data['day'], data['tip'])
 
-# printing the top 10 rows
-st.dataframe(data.head(10))
+# Adding Title to the Plot
+plt.title("Scatter Plot")
+
+# Setting the X and Y labels
+plt.xlabel('Day')
+plt.ylabel('Tip')
+
+plt.show()
+
+# import streamlit as st
+# import pandas as pd
+
+# # Reading the database
+# data = pd.read_csv("https://raw.githubusercontent.com/anggraenideaa/data-visual-2024/master/tips.csv")
+
+
+# # printing the top 10 rows
+# st.dataframe(data.head(10))
+
