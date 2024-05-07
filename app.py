4e023c863eb4faa3887c7cbd704230bbef13b01e
Dea Puspita Anggraeni <125977899+anggraenideaa@users.noreply.github.com>
2024-05-07 15:18:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,3 @@ plt.ylabel('Tip')
 
 plt.show()
 
-# import streamlit as st
-# import pandas as pd
-
-# # Reading the database
-# data = pd.read_csv("https://raw.githubusercontent.com/anggraenideaa/data-visual-2024/master/tips.csv")
-
-
-# # printing the top 10 rows
-# st.dataframe(data.head(10))
-
